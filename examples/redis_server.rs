cb011bfbb869f4935a29b905143980b1c1790c1b
James Courtois <jcourtois@gmail.com>
2023-02-05 16:30:06 -0500

    remove mini-redis dependency

--- examples/redis_server.rs
+++ examples/redis_server.rs
@@ -1,5 +1,4 @@
 use bytes::Bytes;
-use mini_redis::{Connection, Frame};
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 use tokio::net::{TcpListener, TcpStream};
@@ -21,28 +20,28 @@ async fn main() {
     }
 }
 
-async fn process(socket: TcpStream, db: Db) {
-    use mini_redis::Command::{self, Get, Set};
-    let mut connection = Connection::new(socket);
+async fn process(_socket: TcpStream, _db: Db) {
+    // use mini_redis::Command::{self, Get, Set};
+    // let mut connection = Connection::new(socket);
 
-    while let Some(frame) = connection.read_frame().await.unwrap() {
-        let response = match Command::from_frame(frame).unwrap() {
-            Set(cmd) => {
-                let mut db = db.lock().unwrap();
-                db.insert(cmd.key().to_string(), cmd.value().clone());
-                Frame::Simple("OK".to_string())
-            }
-            Get(cmd) => {
-                let db = db.lock().unwrap();
-                if let Some(value) = db.get(cmd.key()) {
-                    Frame::Bulk(value.clone())
-                } else {
-                    Frame::Null
-                }
-            }
-            cmd => panic!("unimplemented {:?}", cmd),
-        };
+    // while let Some(frame) = connection.read_frame().await.unwrap() {
+    //     let response = match Command::from_frame(frame).unwrap() {
+    //         Set(cmd) => {
+    //             let mut db = db.lock().unwrap();
+    //             db.insert(cmd.key().to_string(), cmd.value().clone());
+    //             Frame::Simple("OK".to_string())
+    //         }
+    //         Get(cmd) => {
+    //             let db = db.lock().unwrap();
+    //             if let Some(value) = db.get(cmd.key()) {
+    //                 Frame::Bulk(value.clone())
+    //             } else {
+    //                 Frame::Null
+    //             }
+    //         }
+    //         cmd => panic!("unimplemented {:?}", cmd),
+    //     };
 
-        connection.write_frame(&response).await.unwrap();
-    }
+    //     connection.write_frame(&response).await.unwrap();
+    // }
 }
